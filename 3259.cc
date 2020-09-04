940a52c33fc75c5a2707357eec8f92ed342edfd4
Bichuan Guo <guobichuan@gmail.com>
2020-09-04 15:58:43 +0800

    3259: SPFA

--- 3259.cc
+++ 3259.cc
@@ -31,7 +31,8 @@ typedef long long ll;
 const int INT_INF = 0x3f3f3f3f, INT_NINF = 0xc0c0c0c0;
 const int d4r[] = {0, 1, 0, -1}, d4c[] = {1, 0, -1, 0};
 
-int dist[500][500], spath[500];
+int dist[500][500], spath[500], vis[500];
+bool inqueue[500];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -42,41 +43,39 @@ int main() {
         scanf("%d%d%d", &N, &M, &W);
         memset(dist, 0x3f, sizeof(dist));
         memset(spath, 0x3f, sizeof(spath));
+        memset(vis, 0, sizeof(vis));
+        memset(inqueue, 0, sizeof(inqueue));
         for (int i = 0; i < N; i++) dist[i][i] = 0;
         spath[0] = 0;
-        vector<ii> paths, wormholes;
+        vector<vector<int> > adj(N);
         for (int i = 0; i < M; i++) {
-            scanf("%d%d%d", &s, &e, &t);
+            scanf("%d%d%d", &s, &e, &t); s--, e--;
             dist[s][e] = min(dist[s][e], t);
             dist[e][s] = min(dist[e][s], t);
-            paths.push_back(make_pair(s, e));
+            adj[s].push_back(e);
+            adj[e].push_back(s);
         }
         for (int i = 0; i < W; i++) {
-            scanf("%d%d%d", &s, &e, &t);
+            scanf("%d%d%d", &s, &e, &t); s--, e--;
             dist[s][e] = min(dist[s][e], -t);
-            wormholes.push_back(make_pair(s, e));
+            adj[s].push_back(e);
         }
-        for (int vv = 0; vv < N - 1; vv++) {
-            for (int i = 0; i < paths.size(); i++) {
-                int u = paths[i].first, v = paths[i].second;
-                if (spath[u] + dist[u][v] < spath[v]) spath[v] = spath[u] + dist[u][v];
-                if (spath[v] + dist[v][u] < spath[u]) spath[u] = spath[v] + dist[v][u];
+        queue<int> spfa; bool ncycle = false;
+        spfa.push(0), inqueue[0] = true;
+        while (!spfa.empty() && !ncycle) {
+            int u = spfa.front(); spfa.pop();
+            vis[u]++, inqueue[u] = false;
+            if (vis[u] >= N) {
+                ncycle = true; break;
             }
-            for (int i = 0; i < wormholes.size(); i++) {
-                int u = wormholes[i].first, v = wormholes[i].second;
-                if (spath[u] + dist[u][v] < spath[v]) spath[v] = spath[u] + dist[u][v];
+            for (int j = 0; j < adj[u].size(); j++) {
+                int v = adj[u][j];
+                if (spath[u] + dist[u][v] < spath[v]) {
+                    spath[v] = spath[u] + dist[u][v];
+                    if (!inqueue[v]) spfa.push(v), inqueue[v] = true;
+                }
             }
         }
-        bool ncycle = false;
-        for (int i = 0; i < paths.size() && !ncycle; i++) {
-            int u = paths[i].first, v = paths[i].second;
-            if (spath[u] + dist[u][v] < spath[v] || spath[v] + dist[v][u] < spath[u]) ncycle = true;
-        }
-        for (int i = 0; i < wormholes.size() && !ncycle; i++) {
-            int u = wormholes[i].first, v = wormholes[i].second;
-            if (spath[u] + dist[u][v] < spath[v]) ncycle = true;
-        }
-
         if (ncycle) printf("YES\n");
         else printf("NO\n");
     }
